f35903d17aa855bb1920e99a5a9e276e104385ca
Aaron Stevens <astevens@spp.org>
2019-03-04 15:23:27 -0600

    Rewrote to use useReducer because I was getting weird state flickering issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import { differenceInMonths, startOfMonth } from "date-fns";
 
 import "./App.css";
@@ -28,28 +28,39 @@ const updatePurchases = async setPurchases => {
   setPurchases(purchases);
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "setPurchases":
+      return { ...state, purchases: action.purchases };
+    case "setPurchaseName":
+      return { ...state, purchaseName: action.purchaseName };
+    case "setPurchaseAmount":
+      return { ...state, purchaseAmount: action.purchaseAmount };
+    default:
+      return state;
+  }
+}
+
 function usePurchases() {
   const startDate = startOfMonth(new Date(2019, 1, 28));
   const today = startOfMonth(new Date());
   const monthStartsSinceStartDate = differenceInMonths(today, startDate);
   const startBalance = 125 * monthStartsSinceStartDate;
 
-  const [state, setState] = useState({
+  const [state, dispatch] = useReducer(reducer, {
     startBalance,
     purchaseName: "",
     purchaseAmount: "0.00",
     purchases: []
   });
-
+  
   useEffect(() => {
     updatePurchases(setPurchases);
   }, []);
 
-  const setPurchases = purchases => setState({ ...state, purchases });
-  const setPurchaseName = purchaseName => setState({ ...state, purchaseName });
-  const setPurchaseAmount = purchaseAmount => {
-    setState({ ...state, purchaseAmount });
-  };
+  const setPurchases = purchases => dispatch({ type: 'setPurchases', purchases });
+  const setPurchaseName = purchaseName => dispatch({ type: 'setPurchaseName', purchaseName });
+  const setPurchaseAmount = purchaseAmount => dispatch({ type: 'setPurchaseAmount', purchaseAmount });
 
   const addNewPurchase = () => {
     let purchaseAmount = Number.parseFloat(state.purchaseAmount);
@@ -63,11 +74,8 @@ function usePurchases() {
         purchaseAmount: purchaseAmount
       };
       postPurchase(newPurchase);
-      setState({
-        ...state,
-        purchaseName: "",
-        purchaseAmount: "0.00"
-      });
+      setPurchaseName("");
+      setPurchaseAmount("0.00");
       updatePurchases(setPurchases);
     }
   };
